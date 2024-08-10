f9c25c56dfb7f1900060d912ed9836d89c131c3f
fknraini <fikana19@gmail.com>
2024-08-10 16:45:38 +0700

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'vista-white': '#FAF9F5',
+        'dark-aquaman': '#0B7373',
+        'dark-teal': '#004040',
+        'tan': '#D6B66B',
+      },
+      fontFamily: {
+        'the-signature': ['Thesignature', 'sans-serif'],
+        'unbounded': ['Unbounded', 'sans-serif'],
+      },
+      scrollMargin: {
+        'top-20': '20px',
       },
     },
   },
